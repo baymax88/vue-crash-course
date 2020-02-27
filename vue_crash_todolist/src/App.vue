1ceb129551f2742204543768f69bb88e2b5013e2
Bay <smartbay1@outlook.com>
2020-02-28 07:00:56 +0800

    vue-crash-course-completed

---
+++ vue_crash_todolist/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from './components/layout/Header'
+
+export default {
+  name: "app",
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #fff;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ vue_crash_todolist/src/components/AddTodo.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <input type="text" v-model="title" name="title" placeholder="Add Todo..." />
+            <input type="submit" value="Submit" class="btn" />
+        </form>
+    </div>
+</template>
+
+<script>
+// import { v4 as uuidv4 } from 'uuid'
+
+export default {
+    name: "AddTodo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addTodo(e) {
+            e.preventDefault();
+            const newTodo = {
+                title: this.title,
+                completed: false
+            }
+
+            this.$emit('add-todo', newTodo);
+
+            this.title = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    input[type="text"] {
+        flex: 10;
+        padding: 5px;
+    }
+
+    input[type="submit"] {
+        flex: 2;
+    }
+</style>
